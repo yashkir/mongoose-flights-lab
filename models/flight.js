09f2aa222bfa97b8a3b1cd4dab7dbc009e88edf5
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2021-04-23 17:52:57 -0400

    implement Flight model

---
+++ models/flight.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+
+const flightSchema = new mongoose.Schema({
+  airline: {
+    type: String,
+    enum: ['American', 'Southwest', 'United']
+  },
+  airport: {
+    type: String,
+    enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+    default: 'DEN'
+  },
+  flightNo: {
+    type: Number,
+    min: 10,
+    max: 9999
+  },
+  departs: {
+    type: Date,
+    default: function () {
+      let date = new Date();
+      return date.setUTCFullYear(date.getUTCFullYear() + 1);
+    }
+  }
+});
+
