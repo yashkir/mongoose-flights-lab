bf5322373b4dcd33b5639499476ccd8655a99272
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2021-04-24 16:33:28 -0400

    add /flights/new GET

--- controllers/flights.js
+++ controllers/flights.js
@@ -4,6 +4,11 @@ function index(req, res) {
   res.render('flights/index', { title: "All Flights", flights: Flight.find({}) });
 }
 
+function _new(req, res) {
+  res.render('flights/new', { title: "Add Flight" });
+}
+
 module.exports = {
   index,
+  new: _new,
 }
--- routes/flights.js
+++ routes/flights.js
@@ -4,5 +4,6 @@ var router = express.Router();
 var flightsCtrl = require('../controllers/flights');
 
 router.get('/', flightsCtrl.index);
+router.get('/new', flightsCtrl.new);
 
 module.exports = router;
